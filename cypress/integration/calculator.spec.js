04928a8dcfdec6270722e46fa2499ac180edaf86
parksil0 <sirisiri0809@gmail.com>
2022-03-16 03:34:30 +0900

    refactor : 중복 코드 함수로 분리

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -16,49 +16,49 @@ describe('계산기 기능 요구사항', () => {
   });
 
   it('2개의 숫자에 대해 덧셈이 가능하다', () => {
-    cy.get('.digit').contains('5').click();
-    cy.get('.operation').contains('+').click();
-    cy.get('.digit').contains('3').click();
-    cy.get('.operation').contains('=').click();
+    cy.clickDigit('5');
+    cy.clickOperation('+');
+    cy.clickDigit('3');
+    cy.clickOperation('=');
     cy.get('#total').should('have.text', '8');
   });
 
   it('2개의 숫자에 대해 뺄셈이 가능하다', () => {
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('0').click();
-    cy.get('.operation').contains('-').click();
-    cy.get('.digit').contains('7').click();
-    cy.get('.operation').contains('=').click();
+    cy.clickDigit('1');
+    cy.clickDigit('0');
+    cy.clickOperation('-');
+    cy.clickDigit('7');
+    cy.clickOperation('=');
     cy.get('#total').should('have.text', '3');
   });
 
   it('2개의 숫자에 대해 곱셈이 가능하다', () => {
-    cy.get('.digit').contains('6').click();
-    cy.get('.operation').contains('X').click();
-    cy.get('.digit').contains('4').click();
-    cy.get('.operation').contains('=').click();
+    cy.clickDigit('6');
+    cy.clickOperation('X');
+    cy.clickDigit('4');
+    cy.clickOperation('=');
     cy.get('#total').should('have.text', '24');
   });
 
   it('2개의 숫자에 대해 나눗셈이 가능하다', () => {
-    cy.get('.digit').contains('6').click();
-    cy.get('.operation').contains('/').click();
-    cy.get('.digit').contains('3').click();
-    cy.get('.operation').contains('=').click();
+    cy.clickDigit('6');
+    cy.clickOperation('/');
+    cy.clickDigit('3');
+    cy.clickOperation('=');
     cy.get('#total').should('have.text', '2');
   });
 
   it('AC(All Clear)버튼을 누르면 0으로 초기화된다.', () => {
-    cy.get('.digit').contains('1').click();
+    cy.clickDigit('1');
     cy.get('.modifier').click();
     cy.get('#total').should('have.text', '0');
   });
 
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('1').click();
-    cy.get('.digit').contains('1').click();
+    cy.clickDigit('1');
+    cy.clickDigit('1');
+    cy.clickDigit('1');
+    cy.clickDigit('1');
 
     cy.on('window:alert', (text) => {
       expect(text).to.contains(ERROR_MESSAGE.OVER_THREE_DIGITS);
@@ -67,10 +67,10 @@ describe('계산기 기능 요구사항', () => {
   });
 
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    cy.get('.digit').contains('5').click();
-    cy.get('.operation').contains('/').click();
-    cy.get('.digit').contains('3').click();
-    cy.get('.operation').contains('=').click();
+    cy.clickDigit('5');
+    cy.clickOperation('/');
+    cy.clickDigit('3');
+    cy.clickOperation('=');
     cy.get('#total').should('have.text', '1');
   });
 });
@@ -81,7 +81,7 @@ describe('추가 요구사항', () => {
   });
 
   it('초기 화면에 operator를 클릭하면 alert창을 호출한다.', () => {
-    cy.get('.operation').contains('+').click();
+    cy.clickOperation('+');
 
     cy.on('window:alert', (text) => {
       expect(text).to.contains(ERROR_MESSAGE.NO_NUMBER);
